5b4a1eb2848a20fb8f020372d2773e3863b349b7
Noah Vaknin <122655300+NoVaGamesAndStuff@users.noreply.github.com>
2023-11-29 14:39:28 -0500

    Shop updates

--- Assets/Scripts/ImageFade.cs
+++ Assets/Scripts/ImageFade.cs
@@ -1,50 +1,68 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
- 
-public class ImageFade : MonoBehaviour {
- 
-    // the image you want to fade, assign in inspector
-    public Image img;
 
+public class ImageFade : MonoBehaviour
+{
+    public Image img;
+    public float fadeDuration = 1.0f; // You can adjust this value
 
     public void OnEnable()
     {
-        StartCoroutine(FadeImage(false));
+        StartCoroutine(FadeImage(false, fadeDuration));
     }
 
     public void OnButtonClick()
     {
-        // fades the image out when you click
-        StartCoroutine(FadeImage(true));
+        StartCoroutine(FadeImage(true, fadeDuration));
     }
- 
-    IEnumerator FadeImage(bool fadeAway)
+
+    public void fadeIn()
     {
-        // fade from opaque to transparent
+        StartCoroutine(DarkFadeImage(false, fadeDuration));
+    }
+
+    IEnumerator FadeImage(bool fadeAway, float duration)
+    {
+        float targetAlpha = fadeAway ? 0 : 1;
+        float startAlpha = img.color.a;
+
+        float startTime = Time.time;
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime = Time.time - startTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            img.color = new Color(1, 1, 1, Mathf.Lerp(startAlpha, targetAlpha, t));
+            yield return null;
+        }
+
         if (fadeAway)
         {
-            // loop over 1 second backwards
-            for (float i = 1; i >= 0; i -= Time.deltaTime)
-            {
-                // set color with i as alpha
-                img.color = new Color(1, 1, 1, i);
-                yield return null;
-            }
             this.gameObject.SetActive(false);
         }
-        // fade from transparent to opaque
-        else
-        {
-            // loop over 1 second
-            for (float i = 0; i <= 1; i += Time.deltaTime)
-            {
-                // set color with i as alpha
-                img.color = new Color(1, 1, 1, i);
-                yield return null;
-            }
-           
+    }
 
+    IEnumerator DarkFadeImage(bool fadeAway, float duration)
+    {
+        float targetAlpha = fadeAway ? 0 : 1;
+        float startAlpha = img.color.a;
+
+        float startTime = Time.time;
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime = Time.time - startTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            img.color = new Color(0, 0, 0, Mathf.Lerp(startAlpha, targetAlpha, t));
+            yield return null;
+        }
+
+        if (fadeAway)
+        {
+            this.gameObject.SetActive(false);
         }
     }
 }
