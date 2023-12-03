6a521c162495b616e5fa2ae2f958358d21c9c825
mlitman530 <mlitman530@gmail.com>
2023-12-03 18:32:27 -0500

    Guarantees that a new game clears potion count before shop

--- Assets/Scripts/Menu Scripts/MenuScript.cs
+++ Assets/Scripts/Menu Scripts/MenuScript.cs
@@ -8,9 +8,19 @@ public class MenuScript : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("StatCustomization");
+        ResetPlayerPrefs();
     }
     public void QuitGame()
     {
         Application.Quit();
+    }
+    private void ResetPlayerPrefs()
+    {
+        PlayerPrefs.SetInt("NumHealthPotions", 0);
+        PlayerPrefs.SetInt("NumStrengthPotions", 0);
+        PlayerPrefs.SetInt("NumSpeedPotions", 0);
+        PlayerPrefs.SetInt("NumJumpPotions", 0);
+        PlayerPrefs.SetInt("SwordPurchased", 0);
+        PlayerPrefs.SetInt("NumFirePotions", 0);
     }
 }
